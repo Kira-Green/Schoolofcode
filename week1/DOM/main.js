ab547d0b89d03b13380a06200b6753ddc82075e8
Kira Green <kira.green@btinternet.com>
2019-02-06 15:33:55 +0000

    tidied up

--- week1/DOM/main.js
+++ week1/DOM/main.js
@@ -1,9 +1,10 @@
 // select outer box create new div element
 // set attributes - id =inner , innertext = hello bootcampers
 // append to outer box as a child 
-
-var outer = document.getElementById('outer');
-var inner = document.createElement('div');
-inner.setAttribute('id','inner');
-inner.innerText='Hello Bootcampers';
-outer.appendChild(inner);
+function createOrangeDiv() {
+    var outer = document.getElementById('outer');
+    var inner = document.createElement('div');
+    inner.setAttribute('id','inner');
+    inner.innerText='Hello Bootcampers';
+    outer.appendChild(inner);
+}
