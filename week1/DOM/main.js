d414e117cd42a963c8df03fcf497b48bfb3e0b53
Kira Green <kira.green@btinternet.com>
2019-02-07 13:51:26 +0000

    add

--- week1/DOM/main.js
+++ week1/DOM/main.js
@@ -13,3 +13,24 @@ function changeColor(color) {
     var inner = document.getElementById('inner')
     inner.style.backgroundColor = color;
 }
+
+function buttonClick() {
+
+    console.log("you clicked the button");
+    var input = document.getElementById("myFirstInput");
+    var value = input.value;
+    console.log("My Input Field", input);
+    console.log("My value", value);
+    var output=document.getElementById("output");
+    output.innerHTML = `Hello, ${value}!`;
+
+}
+
+var button = document.getElementById("sayHello");
+button.addEventListener(`click`, buttonClick)  
+// use instead of onclick
+
+var inner = document.getElementById("inner");
+inner.addEventListener(`click`, function() {
+    console.log("inner clicked");
+})
