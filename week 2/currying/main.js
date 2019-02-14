ce2ac5a8e7b93f552703df5382bd680b29f6fec8
Kira Green <kira.green@btinternet.com>
2019-02-14 11:56:25 +0000

    curry

---
+++ week 2/currying/main.js
@@ -0,0 +1,42 @@
+//currying
+//node v9.7.1 linux/amd64   online js compiler https://repl.it/@Kira_Green/TechnologicalPessimisticBooleanalgebra
+
+function sum3(x, y, z) {
+    return x + y + z;
+}
+
+let result = sum3(1, 2, 3)
+console.log(result)
+
+// if one of the numbers is always the same eg
+console.log(sum3(10, 1, 2));
+console.log(sum3(10, 3, 2));
+console.log(sum3(10, 5, 7));
+
+function sum2AndX(x) {
+    return function(a, b) {
+        return sum3(a, b, x);
+    }
+}
+
+const sum2AndTen = sum2AndX(10)
+console.log(sum2AndTen(3, 5))
+
+
+
+//
+
+function curriedsum3(x) {
+    return function (y) {
+        return function (z) {
+            return x + y + z;
+        };
+    };
+}
+
+let curryresult = curriedsum3(1)(2)(5)
+console.log(curryresult)
+
+const alwaysAdd10 = curriedsum3(10)
+let added10 = alwaysAdd10(1)(3)
+console.log(added10)
--- week 2/prototypes/main.js
+++ week 2/prototypes/main.js
@@ -1,3 +1,5 @@
+// constructor function
+
 function Person(name){
     this.name = name;
     this.greeting = function greeting () {
@@ -8,3 +10,16 @@ function Person(name){
 
 var chris = new Person("chris");
 chris.greeting()
+
+function Foo() {
+    this.a = 1;
+    this.b = 2;
+}
+
+var myFoo = new Foo();    // inherits from Foo values for a and b 
+
+Foo.prototype.b = 3;   // updates the prototype to existing but doesn't change values that are there 
+// doesn't really seem to do anything and apparently not used.....
+
+
+
